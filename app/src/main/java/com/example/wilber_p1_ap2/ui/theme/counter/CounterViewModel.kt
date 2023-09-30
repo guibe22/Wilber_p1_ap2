0f7fd943b6c36297237737f9e1778ce94224cfb5
Wilber Galvez <Wilber Galvez@siglo21.local>
2023-09-30 10:30:47 -0400

    first commit

---
+++ app/src/main/java/com/example/wilber_p1_ap2/ui/theme/counter/CounterViewModel.kt
@@ -0,0 +1,23 @@
+package com.example.wilber_p1_ap2.ui.theme.counter
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.wilber_p1_ap2.data.repository.CounterRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class CounterViewModel @Inject constructor(
+    private val repository: CounterRepository
+) : ViewModel() {
+
+    val counter: Flow<Int> = repository.counter
+
+    fun increment() {
+        viewModelScope.launch {
+            repository.increment()
+        }
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "wilber_p1_ap2"
+include(":app")
+ 
